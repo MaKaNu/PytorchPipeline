d1af3384dddcfa293e034653c4c6a0dc575ceecb
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2025-02-04 20:55:27 +0100

    Implement Dataset
    - add implementation of dataset methods
    - update init
    - add Docstring for Pipelineprocess

--- pytorchimagepipeline/pipelines/sam2segnet/permanence.py
+++ pytorchimagepipeline/pipelines/sam2segnet/permanence.py
@@ -1,13 +1,35 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+import cv2
+import torchvision
 from torchvision.datasets import VisionDataset
 
 from pytorchimagepipeline.abstractions import Permanence
+from pytorchimagepipeline.pipelines.sam2segnet.utils import parse_voc_xml
 
 
 @dataclass
 class Datasets(Permanence):
+    """
+    Datasets class is a class which provides torch datasets.
+
+    Example TOML Config:
+    ```toml
+    [permanences.data]
+    type = "Datasets"
+    params = { root = "data/datasets/pascal", format = "pascalvoc" }
+    ```
+
+    Attributes:
+        root (Path): The root directory for the dataset.
+        format (str): The format of the dataset.
+
+    Methods:
+        __post_init__(): Initializes the sam_dataset attribute with a VisionDataset instance.
+        cleanup(): Placeholder method for cleanup operations.
+    """
+
     root: Path
     format: str
 
@@ -19,11 +41,37 @@ class Datasets(Permanence):
 
 
 class SamDataset(VisionDataset):
-    def __init__(self, root=None, transforms=None, transform=None, target_transform=None):
-        super().__init__(root, transforms, transform, target_transform)
+    def __init__(self, root=None):
+        self.root = Path(root)
+
+        self.images = sorted([p for p in (self.root / "Images").iterdir() if p.suffix == ".png"])
+        self.annotations = sorted([p for p in (self.root / "Annotations").iterdir() if p.suffix == ".xml"])
+
+        with (self.root / "classes.json").open() as file_obj:
+            self.class_idx = json.load(file_obj)
+
+        self.target_location = self.root / "SegmentationClassSAM"
 
     def __len__(self):
-        return super().__len__()
+        return len(self.images)
 
     def __getitem__(self, index):
-        return super().__getitem__(index)
+        # Read Image
+        img = cv2.imread(self.images[index])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Read filestem
+        filestem = self.images[index].stem
+
+        # Read Annotations
+        annotation, error = parse_voc_xml(self.annotations[index])
+        if error:
+            raise error
+        bboxes = [obj.bndbox for obj in annotation.objects]
+        bbox_classes = [obj.name for obj in annotation.objects]
+
+        return img, bboxes, bbox_classes, filestem
+
+    def save_item(self, index, mask):
+        mask = mask.squeeze()
+        torchvision.utils.save_image(mask, str(self.target_location / self.images[index].name), "png")
