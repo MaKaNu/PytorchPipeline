a0e60021907049f42fed22e18199883278790aca
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2025-02-04 20:56:57 +0100

    Update sam2segnet register

--- pytorchimagepipeline/pipelines/sam2segnet/__init__.py
+++ pytorchimagepipeline/pipelines/sam2segnet/__init__.py
@@ -1,5 +1,5 @@
-from pytorchimagepipeline.pipelines.sam2segnet.permanence import Datasets
-from pytorchimagepipeline.pipelines.sam2segnet.processes import DummyProcess
+from pytorchimagepipeline.pipelines.sam2segnet.permanence import Datasets, MaskCreator
+from pytorchimagepipeline.pipelines.sam2segnet.processes import PredictMasks
 
-permanences_to_register = {"Datasets": Datasets}
-processes_to_register = {"DummyProcess": DummyProcess}
+permanences_to_register = {"Datasets": Datasets, "MaskCreator": MaskCreator}
+processes_to_register = {"PredictMasks": PredictMasks}
