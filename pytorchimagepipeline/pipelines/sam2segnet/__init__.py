ba75e3a686e25191f05a7cfcfaa9999f6b79338d
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2025-02-06 00:55:17 +0100

    Update sam2segnet Pipeline config

--- pytorchimagepipeline/pipelines/sam2segnet/__init__.py
+++ pytorchimagepipeline/pipelines/sam2segnet/__init__.py
@@ -1,5 +1,5 @@
 from pytorchimagepipeline.pipelines.sam2segnet.permanence import Datasets, MaskCreator
-from pytorchimagepipeline.pipelines.sam2segnet.processes import PredictMasks
+from pytorchimagepipeline.pipelines.sam2segnet.processes import PredictMasks, TrainModel
 
 permanences_to_register = {"Datasets": Datasets, "MaskCreator": MaskCreator}
-processes_to_register = {"PredictMasks": PredictMasks}
+processes_to_register = {"PredictMasks": PredictMasks, "TrainModel": TrainModel}
