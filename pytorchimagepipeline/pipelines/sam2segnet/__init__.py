fcaabf32bda0f0f9460d3421810991fe5998a0a4
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2025-02-08 21:32:49 +0100

    Add new Permanences
    - TrainingComponents
    - HyperParameters
    - Network

--- pytorchimagepipeline/pipelines/sam2segnet/__init__.py
+++ pytorchimagepipeline/pipelines/sam2segnet/__init__.py
@@ -1,5 +1,17 @@
-from pytorchimagepipeline.pipelines.sam2segnet.permanence import Datasets, MaskCreator
+from pytorchimagepipeline.pipelines.sam2segnet.permanence import (
+    Datasets,
+    HyperParameters,
+    MaskCreator,
+    Network,
+    TrainingComponents,
+)
 from pytorchimagepipeline.pipelines.sam2segnet.processes import PredictMasks, TrainModel
 
-permanences_to_register = {"Datasets": Datasets, "MaskCreator": MaskCreator}
+permanences_to_register = {
+    "Datasets": Datasets,
+    "Network": Network,
+    "MaskCreator": MaskCreator,
+    "TrainingComponents": TrainingComponents,
+    "HyperParameters": HyperParameters,
+}
 processes_to_register = {"PredictMasks": PredictMasks, "TrainModel": TrainModel}
