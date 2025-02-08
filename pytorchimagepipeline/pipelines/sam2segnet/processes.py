e1ff382b67c3761fe233863b3f554b2ff95a6834
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2025-02-08 21:29:58 +0100

    Update PredictMasks Process
    - move observer aggregation to constructor
    - update skip accordingly to the changes of SamDataset

--- pytorchimagepipeline/pipelines/sam2segnet/processes.py
+++ pytorchimagepipeline/pipelines/sam2segnet/processes.py
@@ -5,7 +5,7 @@ import torchvision
 from segment_anything import SamPredictor, sam_model_registry
 from tqdm import tqdm
 
-from pytorchimagepipeline.abstractions import PipelineProcess
+from pytorchimagepipeline.abstractions import AbstractObserver, PipelineProcess
 from pytorchimagepipeline.pipelines.sam2segnet.utils import get_palette
 
 
@@ -15,48 +15,51 @@ class PredictMasks(PipelineProcess):
         sam_checkpoint = Path("data/models/sam_vit_h_4b8939.pth")
         model_type = "vit_h"
         self.sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+        self.device = observer.get_permanence("device").device
+        self.dataset = observer.get_permanence("data").sam_dataset
+        self.mask_creator = observer.get_permanence("mask_creator")
 
-    def execute(self, observer):
-        device = observer.get_permanence("device").device
-        self.sam.to(device)
+    def execute(self):
+        self.sam.to(self.device)
         predictor = SamPredictor(self.sam)
 
-        dataset = observer.get_permanence("data").sam_dataset
-
-        bar = tqdm(len(dataset), desc="Predicting masks")
-        for data in dataset:
+        bar = tqdm(len(self.dataset), desc="Predicting masks")
+        for data in self.dataset:
             image, bboxes, bbox_classes, filestem = data
             bbox_classes_idx = torch.tensor(
-                [observer.get_permanence("data").sam_dataset.class_idx[class_] for class_ in bbox_classes],
+                [self.dataset.class_idx[class_] for class_ in bbox_classes],
                 dtype=torch.float32,
-                device=device,
+                device=self.device,
             )
 
             predictor.set_image(image)
 
-            bboxes_tensor = torch.stack([bbox.to_tensor(device=predictor.device) for bbox in bboxes])
-            transformed_boxes = predictor.transform.apply_boxes_torch(bboxes_tensor, image.shape[:2])
-            masks, _, _ = predictor.predict_torch(
-                point_coords=None,
-                point_labels=None,
-                boxes=transformed_boxes,
-                multimask_output=False,
-            )
+            if bboxes:
+                bboxes_tensor = torch.stack([bbox.to_tensor(device=predictor.device) for bbox in bboxes])
+                transformed_boxes = predictor.transform.apply_boxes_torch(bboxes_tensor, image.shape[:2])
+                masks, _, _ = predictor.predict_torch(
+                    point_coords=None,
+                    point_labels=None,
+                    boxes=transformed_boxes,
+                    multimask_output=False,
+                )
 
-            masks_with_classes = masks * bbox_classes_idx.view(-1, 1, 1, 1)
-            merged_mask = observer.get_permanence("mask_creator").create_mask(masks_with_classes)
+                masks_with_classes = masks * bbox_classes_idx.view(-1, 1, 1, 1)
+                merged_mask = self.mask_creator.create_mask(masks_with_classes)
+            else:
+                merged_mask = torch.zeros(image.shape[:2], dtype=torch.uint8, device=self.device)
 
             palette = get_palette()
             mask_as_pil = torchvision.transforms.functional.to_pil_image(merged_mask)
             mask_as_pil.putpalette(palette)
-            mask_path = observer.get_permanence("data").sam_dataset.target_location / f"{filestem}.png"
+            mask_path = self.dataset.target_location / f"{filestem}.png"
             mask_as_pil.save(mask_path)
 
             bar.update(1)
             bar.refresh()
 
     def skip(self):
-        return torch.tensor(list(map(Path.exists, self.dataset.images)), dtype=bool).all() and not self.force
+        return self.dataset.all_created() and not self.force
 
 
 class TrainModel(PipelineProcess):
