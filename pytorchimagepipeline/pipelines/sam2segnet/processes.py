2bdaa5233bf8ebac040bb3d7b1b7d2f5b25a5a07
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2025-02-08 21:31:23 +0100

    Update TrainingProcess
    - was before just scaffholding
    - now uses the all the new permanences and structures

--- pytorchimagepipeline/pipelines/sam2segnet/processes.py
+++ pytorchimagepipeline/pipelines/sam2segnet/processes.py
@@ -65,45 +65,89 @@ class PredictMasks(PipelineProcess):
 class TrainModel(PipelineProcess):
     def __init__(self, observer, force):
         super().__init__(observer, force)
+        self.device = observer.get_permanence("device").device
+        self.model = observer.get_permanence("network").model_instance
+        self.model.to(self.device)
+
+        # Hyperparameters
+        observer.get_permanence("hyperparams").calculate_batch_size(self.device)
+        self.hyperparams = observer.get_permanence("hyperparams").hyperparams
+
+        # Data
+        self.datasets = observer.get_permanence("data")
+        batch_size = self.hyperparams.get("batch_size", 20)
+        trainset = self.datasets.segnet_dataset_train
+        valset = self.datasets.segnet_dataset_val
+        testset = self.datasets.segnet_dataset_test
+
+        self.train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+        self.val_loader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False)
+        self.test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+        # Training components
+        components = observer.get_permanence("training_components")
+        ignore_index = self.datasets.data_container.ignore
+        self.criterion = components.Criterion(**self.hyperparams.get("criterion", {}), ignore_index=ignore_index)
+        self.optimizer = components.Optimizer(self.model.parameters(), **self.hyperparams.get("optimizer", {}))
+        self.scheduler = components.Scheduler(
+            self.optimizer, **self.hyperparams.get("scheduler", components.scheduler_params)
+        )
+        self.num_epochs = self.hyperparams.get("num_epochs", 20)
 
     def skip(self):
         return False
 
     def execute(self):
-        for epoch in range(num_epochs):
-            model.train()
-            running_loss = 0.0
-            for inputs, labels in train_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
+        for epoch in range(self.num_epochs):
+            running_loss = self._train_step()
+            train_msg = f"Epoch {epoch + 1}/{self.num_epochs}, Training Loss: {running_loss / len(self.train_loader)}"
 
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            if self.datasets.val_available():
+                val_loss = self._validate_step(epoch)
+                val_msg = f"Epoch {epoch + 1}/{self.num_epochs}, Validation Loss: {val_loss / len(self.val_loader)}"
+
+        if self.datasets.test_available():
+            test_loss = self._test_step()
+            test_msg = f"Test Loss: {test_loss / len(self.test_loader)}"
+
+    def _train_step(self):
+        self.model.train()
+        running_loss = 0.0
+        for inputs, labels in self.train_loader:
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+            self.optimizer.zero_grad()
+            output = self.model(inputs)
+            main_pred, aux_pred = output.get("out"), output.get("aux", None)
+            main_loss = self.criterion(main_pred, labels)
+            aux_loss = self.criterion(aux_pred, labels) if aux_pred is not None else torch.zeros_like(main_loss)
+            loss = main_loss + self.hyperparams.get("aux_lambda", 0.4) * aux_loss
             loss.backward()
-            optimizer.step()
+            self.optimizer.step()
 
             running_loss += loss.item()
+        self.scheduler.step()
 
-            print(f"Epoch {epoch+1}/{num_epochs}, Training Loss: {running_loss/len(train_loader)}")
+        return running_loss
 
-            model.eval()
-            val_loss = 0.0
-            with torch.no_grad():
-            for inputs, labels in val_loader:
-                inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model(inputs)
-                loss = criterion(outputs, labels)
+    def _validate_step(self):
+        self.model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for inputs, labels in self.val_loader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
                 val_loss += loss.item()
+        return val_loss
 
-            print(f"Epoch {epoch+1}/{num_epochs}, Validation Loss: {val_loss/len(val_loader)}")
-
-        model.eval()
+    def _test_step(self):
+        self.model.eval()
         test_loss = 0.0
         with torch.no_grad():
-            for inputs, labels in test_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            test_loss += loss.item()
-
-        print(f"Test Loss: {test_loss/len(test_loader)}")
+            for inputs, labels in self.test_loader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+                test_loss += loss.item()
+        return test_loss
